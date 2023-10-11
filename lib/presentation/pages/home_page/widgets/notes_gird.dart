0cae14e06f2a559654c589c6cb2f5e344ecbe93c
vishnucprasad <mail@vishnucprasad.in>
2023-10-11 09:57:02 +0530

    Setup empty notes list UI

--- lib/presentation/pages/home_page/widgets/notes_gird.dart
+++ lib/presentation/pages/home_page/widgets/notes_gird.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:notable/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:notable/presentation/core/constants.dart';
 import 'package:notable/presentation/pages/home_page/widgets/notes_grid_item.dart';
 import 'package:notable/presentation/router/app_router.dart';
 
@@ -25,30 +26,54 @@ class NotesGird extends StatelessWidget {
               loading: (state) => const Center(
                 child: CircularProgressIndicator(),
               ),
-              loadSuccess: (state) => SingleChildScrollView(
-                child: StaggeredGrid.count(
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 2,
-                  crossAxisSpacing: 2,
-                  children: List.generate(
-                    state.notes.size,
-                    (index) => GestureDetector(
-                      onPanDown: (_) => context
-                          .pushRoute(ViewNoteRoute(note: state.notes[index])),
-                      child: NotesGridItem(
-                        note: state.notes[index],
-                        color: Color(
-                          int.parse(
-                                state.notes[index].colorString.substring(1, 7),
-                                radix: 16,
-                              ) +
-                              0xFF000000,
+              loadSuccess: (state) {
+                return state.notes.size == 0
+                    ? Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              height: 100,
+                              width: 100,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                    'assets/icon/notable_icon.png',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const Text('Notes you add appear here'),
+                            kHeight,
+                          ],
                         ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
+                      )
+                    : SingleChildScrollView(
+                        child: StaggeredGrid.count(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 2,
+                          crossAxisSpacing: 2,
+                          children: List.generate(
+                            state.notes.size,
+                            (index) => GestureDetector(
+                              onPanDown: (_) => context.pushRoute(
+                                  ViewNoteRoute(note: state.notes[index])),
+                              child: NotesGridItem(
+                                note: state.notes[index],
+                                color: Color(
+                                  int.parse(
+                                        state.notes[index].colorString
+                                            .substring(1, 7),
+                                        radix: 16,
+                                      ) +
+                                      0xFF000000,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+              },
               loadFailure: (state) => const Center(
                 child: Text('Load error'),
               ),
